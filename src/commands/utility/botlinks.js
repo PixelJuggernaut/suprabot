e449f9f61a5e62982bcf956a55caffcd3351f9d0
David Markovic <psycho006business@gmail.com>
2023-07-24 14:40:21 +0200

    added

---
+++ src/commands/utility/botlinks.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+    message.delete()
+
+    let bot = new Discord.RichEmbed()
+        .setAuthor("Zeltux","https://cdn.discordapp.com/attachments/632238663094370366/632916675808854026/profile.png")
+        .setTitle("Supra - Quick Links")
+        .setColor(`#FF0062`)
+        .addField(`Website`,`[Website](https://suprabot.vercel.app/)`, true)
+        .addField(`GitHub`,`[Github](https://github.com/PixelJuggernaut/suprabot-informations)`, true)
+        .addField(`Discord`,`[Discord](https://discord.gg/pk8Ve68UEH)`, true)
+        .setFooter(`© Supra Bot | Owned by PixelJuggernaut | Developed by PixelJuggernaut\n${client.config.serverName} Utilities ➤ Command ran by ${message.author.username}`,"https://cdn.discordapp.com/attachments/632238663094370366/632916675808854026/profile.png")
+
+    message.channel.send(bot)
+}
